a730d55c4e47192483c27df389ac91ada85334ec
walz0 <aidan.m.walz@gmail.com>
2022-05-24 22:55:33 -0400

    added hidden input field to auth script

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 import base64
+from getpass import getpass
 
-with open(".env", "w") as file:
+with open(".env", "w+") as file:
     username = input("username: ")
-    password = input("password: ")
+    password = getpass("password: ")
     file.write("GITHUB_AUTH=" + str(base64.b64encode(bytes((username + ":" + password), "utf-8")))[2:-1])
